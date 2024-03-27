0a4dedba2dceb77abc7cf3f6401a9855e4ef7109
Bolatito2023 <forewaaboderin@yahoo.com>
2024-03-26 17:08:20 -0700

    I added some new functionalities

--- src/main/java/org/example/FitnessApp.java
+++ src/main/java/org/example/FitnessApp.java
@@ -3,6 +3,8 @@ package org.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Objects;
 import java.util.Scanner;
 import java.sql.Date;
 
@@ -13,6 +15,11 @@ public class FitnessApp {
     private static int portNumber;
 
     private static Member member;
+    private static Staff staff;
+
+    private static Trainer trainer;
+
+
 
     public String getUsername() { return username; }
     public String getPassword() { return password; }
@@ -26,7 +33,10 @@ public class FitnessApp {
         username = "postgres";
         password = "gbemisola23$";
         portNumber = 2244;
+
         member = new Member();
+        staff = new Staff();
+        trainer = new Trainer();
     }
 
     /**
@@ -50,6 +60,8 @@ public class FitnessApp {
     public static void main(String[] args){
         FitnessApp app = new FitnessApp();
         Scanner scanner = new Scanner(System.in);
+        Integer tempId;
+        Integer currentId = null;
 
         System.out.println("Current DataBase Configuration:");
         System.out.println("Username:    " + app.getUsername()   + "\n" +
@@ -99,7 +111,7 @@ public class FitnessApp {
         while (true) {
             System.out.println("""
                     Welcome to Health and Fitness Club!
-                    1) Member Registration
+                    1) Registration
                     2) Trainer Login
                     3) Staff Login
                     4) Member Login
@@ -107,35 +119,72 @@ public class FitnessApp {
             String option = scanner.nextLine().trim();
             switch (option) {
                 case "1":
-                    System.out.println("Enter your First name:");
-                    String first_name = scanner.nextLine();
-                    System.out.println("Enter your Last name:");
-                    String last_name = scanner.nextLine();
-                    System.out.println("Enter your Email name:");
-                    String email = scanner.nextLine();
-                    System.out.println("Enter your Join Date (yyyy-mm-dd):");
-                    String joinDateInput = scanner.nextLine(); // Read the date input from the user
-                    Date joinDate = Date.valueOf(joinDateInput); // Parse the input string into a Date object
-                    System.out.println("Enter your password:");
-                    String password = scanner.nextLine();
-
-                    member.memberRegister(first_name,last_name,email,joinDate ,password);
                     System.out.println("\n");
-                        break;
+                    System.out.println("1) Member Registration\n" +
+                            "2) Trainer Registration\n" +
+                            "3) Staff Registration\n" +
+                            "0) Return");
+                    String reg_option = scanner.nextLine();
+                    if(Objects.equals(reg_option, "0")) break;
+                    System.out.println("Enter your First name:");
+                    String reg_fn = scanner.nextLine();
+                    System.out.println("Enter your Last name:");
+                    String reg_ln = scanner.nextLine();
+                    System.out.println("Enter your Email name:");
+                    String reg_email = scanner.nextLine();
+                    String reg_speciality = "";
+                    if(Objects.equals(reg_option, "2")) {
+                        System.out.println("Enter your Speciality:");// This is only asked if the user is a trainer
+                        reg_speciality = scanner.nextLine();
+                    }
+                    System.out.println("Enter your password:");
+                    String reg_password = scanner.nextLine();
+                    switch(reg_option) {
+                        case "1":
+                            Member.memberRegister(reg_fn,reg_ln,reg_email, Date.valueOf(LocalDate.now()) ,reg_password);
+                            break;
+                        case "2":
+                            Trainer.trainerRegister(reg_fn, reg_ln, reg_email,reg_speciality, reg_password);
+                            break;
+                        case "3":
+                            Staff.staffRegister(reg_fn, reg_ln, reg_email, reg_password);
+                        default:
+                            System.out.println("Invalid option, please try again.");
+                    }
 
+
+                    break;
                 case "2":
+                    System.out.println("\n");
+                    System.out.println("Enter your Email name:");
+                    String tEmail_login = scanner.nextLine();
+
+                    System.out.println("Enter your password:");
+                    String tPassword_login = scanner.nextLine();
+                    tempId = Trainer.trainerLogin(tEmail_login,tPassword_login);
+                    if(tempId != null) { currentId = tempId; }
+                    trainerFunction();
+                    //Staff.CheckMaintenance(currentId); //Just to test the function
+                    break;
 
                 case "3":
-                    // Handle staff login
+                    System.out.println("\n");
+                    System.out.println("Enter your Email name:");
+                    String staffLogin_email = scanner.nextLine();
+                    System.out.println("Enter your password:");
+                    String staffLogin_password = scanner.nextLine();
+                    tempId = Staff.staffLogin(staffLogin_email,staffLogin_password);
+                    if(tempId != null) { currentId = tempId; }
+                    //Staff.CheckMaintenance(currentId); //Just to test the function
                     break;
                 case "4":
                     System.out.println("Enter your Email name:");
-                    String email_login = scanner.nextLine();
-
+                    String memberLogin_email = scanner.nextLine();
                     System.out.println("Enter your password:");
-                    String password_login = scanner.nextLine();
-                    member.memberLogin(email_login,password_login);
-                    System.out.println("\n");
+                    String memberLogin_password = scanner.nextLine();
+                    tempId = Member.memberLogin(memberLogin_email,memberLogin_password);
+                    if(tempId != null) { currentId = tempId; memberFunction(currentId); }
+
                     break;
                 case "0":
                     System.out.println("Exiting program.");
@@ -145,4 +194,78 @@ public class FitnessApp {
             }
         }
     }
+
+    /**
+     * This function will be called once the trainer has logged in
+     * They have the option to set availability or view
+     */
+
+    public static void trainerFunction(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n");
+        System.out.println("Welcome " + trainer.getFnName() +"\n"+ """
+                    1) Set Availability
+                    2) View Member Profile
+                    0) Exit""");
+
+        String option = scanner.nextLine().trim();
+        switch (option) {
+            case "1":
+                System.out.println("Enter your day of the week");
+                String dayOfWeek = scanner.nextLine();
+
+                System.out.println("Enter your time period");
+                System.out.println("e.g 11:30am - 1:00pm");
+                String timePeriod = scanner.nextLine();
+                Trainer.scheduleAvailabilty(Trainer.getFnName(),Trainer.getLnName(),Trainer.getSpeciality(),dayOfWeek,timePeriod);
+                break;
+            case "2":
+                break;
+                //Trainer.memberSearch();
+            case "0":
+                return; // Exit the trainer function and return to the main menu
+            default:
+                System.out.println("Invalid option, please try again.");
+        }
+    }
+
+    /**
+     * This method is called when the member successfully logs in
+     * More options will be added such as viewing fitness goals e.t.c
+     */
+    public static void memberFunction(Integer id){
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n");
+            System.out.println("Welcome " + Member.getMemberName() + "\n" +
+                    "1) View Trainer Schedule\n" +
+                    "2) Update Information\n" +
+                    "0) Exit");
+
+
+            String option = scanner.nextLine().trim();
+            switch (option) {
+                case "1":
+                    Member.getTrainerSchedule();
+                    break;
+                case "2":
+                    System.out.println("Enter your new First name:");
+                    String new_fn = scanner.nextLine();
+                    System.out.println("Enter your new Last name:");
+                    String new_ln = scanner.nextLine();
+                    System.out.println("Enter your new Email name:");
+                    String new_email = scanner.nextLine();
+                    System.out.println("Enter your new password:");
+                    String new_password = scanner.nextLine();
+
+                    Member.updatePersonalInformation(new_fn,new_ln,new_email,new_password,id);
+                    break;
+                case "0":
+                    return; // Exit the member function and return to the main menu
+                default:
+                    System.out.println("Invalid option, please try again.");
+            }
+        }
+    }
+
 }
--- src/main/java/org/example/Trainer.java
+++ src/main/java/org/example/Trainer.java
@@ -1,6 +1,184 @@
 package org.example;
 
-import java.time.LocalDate;
-import java.sql.*;
 import org.mindrot.jbcrypt.BCrypt;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Trainer{
+    //This will tell if the password entered during log in is verified or not
+    private static boolean read = false;
+    //These are made so that when trainer wants to set availabilty after logging in
+    //the database will automatically put its info and just ask for day of the week and time period
+    private static String trainerFnName = "";
+    private static String trainerLnName = "";
+    private static String trainerEmail = "";
+    private static String trainerSpeciality = "";
+
+
+
+    /**
+     * Add a trainer to table "trainer" with specified information.
+     * @param fn represents the member's first name.
+     * @param ln represents the member's last name.
+     * @param email represents the member's email address, this is unique.
+     * @param password_hash represents the staff's password.
+     */
+    public static void trainerRegister(String fn, String ln, String email,String speciality,String password_hash) {
+        String query = "INSERT INTO trainer (firstname, lastname, email,speciality,  password_hash) VALUES (?, ?, ?, ?, ?)";
+
+        try {
+            Connection connect = FitnessApp.getConnection();
+            PreparedStatement ps = connect.prepareStatement(query);
+
+            ps.setString(1, fn);
+            ps.setString(2, ln);
+            ps.setString(3, email);
+            ps.setString(4,speciality);
+            ps.setString(5, BCrypt.hashpw(password_hash, BCrypt.gensalt()));
+            ps.executeUpdate();
+            System.out.println("New Trainer registered.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method will help user to log into the Fitness App
+     */
+    /**
+     * Log in as a staff, email and password required.
+     * @return the staff's id
+     */
+    public static Integer trainerLogin(String email, String password) {
+        String storedHashedPassword = null;
+
+        try {
+            Connection connect = FitnessApp.getConnection();
+
+            PreparedStatement ps = connect.prepareStatement("SELECT * FROM trainer WHERE email = ?");
+
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                trainerFnName = rs.getString("firstname");
+                trainerLnName = rs.getString("lastname");
+                trainerEmail = rs.getString("email");
+                trainerSpeciality=  rs.getString("speciality");
+                storedHashedPassword = rs.getString("password_hash");
+            }
+
+            if (storedHashedPassword != null) {
+                //This will tell if the password entered during log in is verified or not
+                boolean read = BCrypt.checkpw(password, storedHashedPassword);
+                if (read) {
+                    System.out.println("Logged in successfully.");
+                    return (rs.getInt("trainer_id"));
+                } else {
+                    System.out.println("Incorrect password.");
+                    return null;
+                }
+            } else {
+                System.out.println("No matching email found.");
+                return null;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    // should search for members in another table( registered with particular trainer) not members table should
+    // search for people who are in their session
+    public static List<MemberProfile> memberSearch(String first_name, String last_name){
+        List<MemberProfile> members = new ArrayList<>();
+        String query = "SELECT * FROM members WHERE first_name = ? OR last_name = ?";
+        try {
+            Connection connection = FitnessApp.getConnection();
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, first_name);
+            ps.setString(2, last_name);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                // Map the retrieved data to MemberProfile objects
+                int memberId = rs.getInt("member_id");
+                String memberFirstName = rs.getString("first_name");
+                String memberLastName = rs.getString("last_name");
+                String email = rs.getString("email");
+                // Create a MemberProfile object and add it to the list
+                members.add(new MemberProfile(memberId, memberFirstName, memberLastName, email));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return members;
+    }
+
+    /**
+     * Should be in a different table not trainers the members should look at that table to book a session
+     * @param firstname
+     * @param lastname
+     * @param specialty
+     * @param dayOfWeek
+     */
+
+    public static void scheduleAvailabilty( String firstname, String lastname,String specialty,
+                                     String dayOfWeek,String timePeriod) {
+        String query = "INSERT INTO trainerSchedule (firstname, lastname, speciality, dayOfWeek," +
+                "timePeriod) VALUES (?, ?, ?, ?, ?)";
+        try {
+            Connection connect = FitnessApp.getConnection();
+            PreparedStatement ps = connect.prepareStatement(query);
+
+            ps.setString(1, firstname);
+            ps.setString(2, lastname);
+            ps.setString(3, specialty);
+            ps.setString(4, dayOfWeek);
+            ps.setString(5, timePeriod);
+            ps.executeUpdate();
+            System.out.println("Successfully set Availability");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error setting availability.");
+        }
+    }
+
+    /**
+     * get trainers first name
+     * @return String
+     */
+
+    public static String getFnName(){
+        return trainerFnName;
+    }
+
+    /**
+     * get trainers last name
+     * @return String
+     */
+
+    public static String getLnName(){
+        return trainerLnName;
+    }
+
+    /**
+     * get trainers speciality
+     * @return String
+     */
+    public static String getSpeciality(){
+        return trainerSpeciality;
+    }
+
+
+
+
+
+
+
+
+}
